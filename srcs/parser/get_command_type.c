34cbf3858f1ee8eba0ff2dd68d26ce3d2de32485
cwindom <cwindom@users.noreply.github.com>
2020-11-09 03:25:10 +0400

    exec_external_or_builtin_command.c Error (line 60): function exec_external_command has 32 lines

---
+++ srcs/parser/get_command_type.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_command_type.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwindom <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/09 03:21:31 by cwindom           #+#    #+#             */
+/*   Updated: 2020/11/09 03:21:35 by cwindom          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+#include "parser.h"
+
+E_COMMAND	get_command_type(char *command_name)
+{
+	E_COMMAND	i;
+	int			len;
+	char		*commands[8];
+	E_COMMAND	res;
+
+	commands[COMMAND_CD] = "cd";
+	commands[COMMAND_ECHO] = "echo";
+	commands[COMMAND_EXPORT] = "export";
+	commands[COMMAND_EXIT] = "exit";
+	commands[COMMAND_ENV] = "env";
+	commands[COMMAND_PWD] = "pwd";
+	commands[COMMAND_UNSET] = "unset";
+	len = ft_strlen(command_name) + 1;
+	i = COMMAND_EXTERNAL;
+	res = COMMAND_EXTERNAL;
+	while (++i < NONE_COMMAND && res == COMMAND_EXTERNAL)
+		if (ft_strncmp(command_name, commands[i], len) == 0)
+			res = i;
+	return (res);
+}
