17f19b9609656ff165d28796a79bbefb4b06e1ce
kallard <kallard@student.21-school.ru>
2020-11-08 15:12:44 +0300

    Norme: command_cd

--- srcs/commands/command_cd.c
+++ srcs/commands/command_cd.c
@@ -10,51 +10,65 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../minishell.h"
+#include "../minishell.h"
 
-int command_cd(char **argv, t_list *envs)
+static int	cd_path(char *path, char *pwd, t_list *envs)
 {
-	DIR		*dir;
-	char	*path;
-	char	*pwd;
-	char	*is_cwd;
-    int		res;
+	DIR	*dir;
+	int	res;
 
 	res = 0;
-	path = (count_argv(argv) > 1) ? argv[1] : get_env_value("HOME", envs);
-	if (path == NULL)
-		return (17);
-	pwd = get_env_value("PWD", (t_list *)envs);
+	dir = opendir(path);
+	if (dir != NULL)
+	{
+		if ((chdir(path) < 0) || (closedir(dir) < 0))
+			res = 1;
+		if (res == 0)
+		{
+			update_env_data(envs, "OLDPWD", pwd);
+			pwd = getcwd(0, 1024);
+			update_env_data(envs, "PWD", pwd);
+		}
+	}
+	else
+		res = 1;
+	return (res);
+}
+
+static void	cd_point(char *pwd, t_list *envs)
+{
+	char	*is_cwd;
+
 	is_cwd = getcwd(0, 1024);
-	if (!is_cwd && path[0] == '.' && path[1] == '\0')
+	if (!is_cwd)
 	{
 		update_env_data(envs, "OLDPWD", pwd);
 		pwd = ft_strjoin(pwd, "/.");
 		update_env_data(envs, "PWD", pwd);
 		free(pwd);
 	}
-	else if(path[0] == '.' && path[1] == '\0')
+	else
 	{
 		update_env_data(envs, "OLDPWD", pwd);
 		update_env_data(envs, "PWD", pwd);
 	}
+}
+
+int			command_cd(char **argv, t_list *envs)
+{
+	char	*path;
+	char	*pwd;
+	int		res;
+
+	res = 0;
+	path = (count_argv(argv) > 1) ? argv[1] : get_env_value("HOME", envs);
+	if (path == NULL)
+		return (17);
+	pwd = get_env_value("PWD", (t_list *)envs);
+	if (path[0] == '.' && path[1] == '\0')
+		cd_point(pwd, envs);
 	else
-	{
-		dir = opendir(path);
-		if (dir != NULL)
-		{
-			if ((chdir(path) < 0) || (closedir(dir) < 0))
-				res = 1;
-			if (res == 0)
-			{
-				update_env_data(envs, "OLDPWD", pwd);
-				pwd = getcwd(0, 1024);
-				update_env_data(envs, "PWD", pwd);
-			}
-		}
-		else
-			res = 1;
-	}
+		res = cd_path(path, pwd, envs);
 	if (res == 1)
 	{
 		ft_putstr_fd("cd: ", STDERR_FILENO);
