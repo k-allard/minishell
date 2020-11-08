fbe26e4c1e2598c4a1e05e2160d4b617a6a616b0
kallard <kallard@student.21-school.ru>
2020-11-08 15:52:27 +0300

    Norme: srcs/t_command/t_redirects_close.h

--- srcs/t_command/t_redirects_close.h
+++ srcs/t_command/t_redirects_close.h
@@ -1,18 +1,28 @@
-#ifndef FT_MINISHELL_T_REDIRECTS_CLOSE_H
-#define FT_MINISHELL_T_REDIRECTS_CLOSE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_redirects_close.h                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/08 15:49:46 by kallard           #+#    #+#             */
+/*   Updated: 2020/11/08 15:49:47 by kallard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "../minishell.h"
+#ifndef T_REDIRECTS_CLOSE_H
+# define T_REDIRECTS_CLOSE_H
 
+# include "../minishell.h"
 
-typedef struct s_command
+typedef struct		s_command
 {
-    t_list_lexema *command_with_args;
-    int stdout_fd;
-    int stdin_fd;
-} t_command;
+	t_list_lexema	*command_with_args;
+	int				stdout_fd;
+	int				stdin_fd;
+}					t_command;
 
-t_command* t_command_init();
-void t_command_free(t_command *command);
+t_command			*t_command_init();
+void				t_command_free(t_command *command);
 
-
-#endif //FT_MINISHELL_T_REDIRECTS_CLOSE_H
+#endif
