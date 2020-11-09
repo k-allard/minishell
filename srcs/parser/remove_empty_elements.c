bd52b3c4ccd04622ff8e3e7dcfa0d0907f694c5e
kallard <kallard@student.21-school.ru>
2020-11-09 22:35:08 +0300

    Norme: parser/remove_empty_elements.c

--- srcs/parser/remove_empty_elements.c
+++ srcs/parser/remove_empty_elements.c
@@ -13,12 +13,65 @@
 #include "../minishell.h"
 #include "parser.h"
 
-void	remove_empty_elements(t_list_lexema **lexema_chain)
+static void	second_func(t_list_lexema *current, t_list_lexema *prev)
+{
+	char			*tmp_string;
+	t_list_lexema	*tmp;
+
+	if ((current->next->lexema->has_space_before) && \
+		(current->next->lexema->string[0] != '\0'))
+	{
+		tmp_string = current->next->lexema->string;
+		current->next->lexema->string = ft_strjoin(" ", \
+										current->next->lexema->string);
+		free(tmp_string);
+	}
+	else if (!(current->lexema->has_space_before) && prev)
+	{
+		tmp_string = prev->lexema->string;
+		prev->lexema->string = ft_strjoin(prev->lexema->string, \
+								current->next->lexema->string);
+		free(tmp_string);
+		tmp = current->next;
+		current->next = current->next->next;
+		free(tmp->lexema->string);
+		free(tmp->lexema);
+		free(tmp);
+	}
+}
+
+static void	third_func(t_list_lexema **current, t_list_lexema *prev, \
+						t_list_lexema **lexema_chain)
+{
+	t_list_lexema	*tmp;
+
+	if ((*current)->lexema->lexema_type == lexema_type_simple_word)
+	{
+		if (prev == NULL)
+			*lexema_chain = (*current)->next;
+	}
+	else
+	{
+		if ((*current)->next)
+			second_func(*current, prev);
+	}
+	tmp = *current;
+	*current = (*current)->next;
+	if (prev)
+	{
+		prev->next = *current;
+		*current = prev;
+	}
+	free(tmp->lexema->string);
+	free(tmp->lexema);
+	free(tmp);
+	tmp = NULL;
+}
+
+void		remove_empty_elements(t_list_lexema **lexema_chain)
 {
 	t_list_lexema	*prev;
 	t_list_lexema	*current;
-	t_list_lexema	*tmp;
-	char			*tmp_string;
 
 	prev = NULL;
 	current = *lexema_chain;
@@ -26,53 +79,7 @@ void	remove_empty_elements(t_list_lexema **lexema_chain)
 	{
 		if (current->lexema->string[0] == '\0')
 		{
-			if (current->lexema->lexema_type == lexema_type_simple_word)
-			{
-				if (prev == NULL)
-				{
-					*lexema_chain = current->next;
-				}
-			}
-			else
-			{
-				if (current->next)
-				{
-					if (current->next->lexema->has_space_before)
-					{
-						if ((current->next->lexema->string[0] != '\0'))
-						{
-							tmp_string = current->next->lexema->string;
-							current->next->lexema->string = ft_strjoin(" ", current->next->lexema->string);
-							free(tmp_string);
-						}
-					}
-					else
-					{
-						if (!(current->lexema->has_space_before) && prev)
-						{
-							tmp_string = prev->lexema->string;
-							prev->lexema->string = ft_strjoin(prev->lexema->string, current->next->lexema->string);
-							free(tmp_string);
-							tmp = current->next;
-							current->next = current->next->next;
-							free(tmp->lexema->string);
-							free(tmp->lexema);
-							free(tmp);
-						}
-					}
-				}
-			}
-			tmp = current;
-			current = current->next;
-			if (prev)
-			{
-				prev->next = current;
-				current = prev;
-			}
-			free(tmp->lexema->string);
-			free(tmp->lexema);
-			free(tmp);
-			tmp = NULL;
+			third_func(&current, prev, lexema_chain);
 			continue;
 		}
 		prev = current;
