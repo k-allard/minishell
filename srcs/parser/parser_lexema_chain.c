731c0989d14cc5da0d075871eb27405e6f8c42c9
kallard <kallard@student.21-school.ru>
2020-11-09 22:48:26 +0300

    Norme: parser/parser_lexema_chain.c

--- srcs/parser/parser_lexema_chain.c
+++ srcs/parser/parser_lexema_chain.c
@@ -12,14 +12,26 @@
 
 #include "../minishell.h"
 
-t_list_lexema	*get_next_lexema_chain(t_list_lexema **lexema_list,\
-t_lexema_type delimeter)
+static void		delete_list(t_list_lexema **lexema_list, \
+				t_list_lexema **prev_node)
+{
+	t_list_lexema	*lexema_list_tmp;
+
+	lexema_list_tmp = (*lexema_list)->next;
+	(*prev_node)->next = NULL;
+	free((*lexema_list)->lexema->string);
+	free((*lexema_list)->lexema);
+	free((*lexema_list));
+	*lexema_list = lexema_list_tmp;
+}
+
+t_list_lexema	*get_next_lexema_chain(t_list_lexema **lexema_list, \
+				t_lexema_type delimeter)
 {
 	t_lexema		*lexema;
 	int				is_not_delimeter_prev;
 	t_list_lexema	*chain;
 	t_list_lexema	*prev_node;
-	t_list_lexema	*lexema_list_tmp;
 
 	prev_node = NULL;
 	chain = *lexema_list;
@@ -34,12 +46,7 @@ t_lexema_type delimeter)
 		}
 		else
 		{
-			lexema_list_tmp = (*lexema_list)->next;
-			prev_node->next = NULL;
-			free((*lexema_list)->lexema->string);
-			free((*lexema_list)->lexema);
-			free((*lexema_list));
-			*lexema_list = lexema_list_tmp;
+			delete_list(lexema_list, &prev_node);
 			is_not_delimeter_prev = 0;
 		}
 	}
