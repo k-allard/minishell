fe07694dc0264099cf0446d06e542d3cb642b7f7
kallard <kallard@student.21-school.ru>
2020-11-10 21:55:41 +0300

    SHLVL env variable value is updated now

--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,33 @@
 /*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 22:38:46 by kallard           #+#    #+#             */
-/*   Updated: 2020/11/09 17:09:04 by kallard          ###   ########.fr       */
+/*   Updated: 2020/11/10 21:53:26 by kallard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "parser/parser.h"
 
-int		main(int argc, char **argv, char **envp)
+static void	update_shlvl(t_list_env *envs)
+{
+	char	*shlvl_str;
+	int		shlvl;
+
+	shlvl_str = get_env_value("SHLVL", (t_list *)envs);
+	if (shlvl_str[0] == '\0')
+	{
+		shlvl_str = "1";
+		update_env_data((t_list *)envs, "SHLVL", shlvl_str);
+		return ;
+	}
+	shlvl = ft_atoi(shlvl_str);
+	shlvl++;
+	shlvl_str = ft_itoa(shlvl);
+	update_env_data((t_list *)envs, "SHLVL", shlvl_str);
+	free(shlvl_str);
+}
+
+int			main(int argc, char **argv, char **envp)
 {
 	t_args_struct	args_struct;
 	char			*line;
@@ -22,6 +41,7 @@ int		main(int argc, char **argv, char **envp)
 	args_struct.argc = argc;
 	args_struct.argv = argv;
 	args_struct.envs = (t_list_env *)get_envs(envp);
+	update_shlvl(args_struct.envs);
 	signals();
 	if (argc == 3 && ft_strncmp(argv[1], "-c", 3) == 0)
 		res = parser(argv[2], &args_struct);
